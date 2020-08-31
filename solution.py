935c5d02391fa6cd5cf928b0de5bb6daee1a3470
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2020-08-31 19:59:44 -0400

    Change to map list for the split to typecast int

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 # Take the two input arrays and saves them to a and b respectively
-firstArray = input().split()
-secondArray = input().split()
+firstArray = list(map(int, input.split()))
+secondArray = list(map(int, input.split()))
 finalArray = []
 
 # Initialize the index values
