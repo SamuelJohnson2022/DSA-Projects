96493a11b1a59ded0858d21238d307e410dc71de
Nikita Petrenko <thenikitapetrenko@gmail.com>
2020-08-31 19:36:32 -0400

    Fix arrays

--- solution.py
+++ solution.py
@@ -1,6 +1,10 @@
+import sys
+
 # Take the two input arrays and saves them to a and b respectively
 firstArray = input().split()
 secondArray = input().split()
+firstArray.append(sys.maxsize)
+secondArray.append(sys.maxsize)
 finalArray = []
 
 # Initialize the index values
@@ -9,7 +13,7 @@ secondIndex = 1
 twoIndex = 0
 
 # Loops through both arrays and add the values in ascending order by comparison
-for twoIndex in range(len(firstArray) + len(secondArray) - 2):
+for twoIndex in range(len(firstArray) + len(secondArray) - 4):
     if firstArray[firstIndex] <= secondArray[secondIndex]:
         finalArray.append(firstArray[firstIndex])
         firstIndex += 1
