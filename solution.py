9a41f629b7523d63407f1d82e5ba7c56a0d6b85d
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2020-08-31 19:26:17 -0400

    implement 2 index

--- solution.py
+++ solution.py
@@ -1,6 +1,20 @@
 # Take the two input arrays and saves them to a and b respectively
-a = input().split()
-b = input().split()
+firstArray = input().split()
+secondArray = input().split()
+finalArray = []
 
-print(a)
-print(b)
+# Initialize the index values
+firstIndex = 1
+secondIndex = 1
+twoIndex = 0
+
+# Loops through both arrays and add the values in ascending order by comparison
+for twoIndex in range(len(firstArray) + len(secondArray) - 2):
+    if firstArray[firstIndex] <= secondArray[secondIndex]:
+        finalArray.append(firstArray[firstIndex])
+        firstIndex += 1
+    else:
+        finalArray.append(secondArray[secondIndex])
+        secondIndex += 1
+
+print(finalArray)
