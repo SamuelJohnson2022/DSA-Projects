2730fbcc834f3fcb1fc39c3d7967331caad2faf5
Nikita Petrenko <thenikitapetrenko@gmail.com>
2020-08-31 21:15:37 -0400

    add problem 2 code

--- solution.py
+++ solution.py
@@ -1,29 +1,45 @@
 # Collaborators: Samuel Johnson, Nikita Petrenko, Jason Novillo
-
 import sys
-# Take the two input arrays and saves them to a and b respectively
-firstArray = list(map(int, input().split()))
-secondArray = list(map(int, input().split()))
 
-firstArray.append(sys.maxsize)
-secondArray.append(sys.maxsize)
-finalArray = []
+def merge_sorted(firstArray, secondArray):
+    firstArray.append(sys.maxsize)
+    secondArray.append(sys.maxsize)
+    finalArray = []
 
-# Initialize the index values
-firstIndex = 1
-secondIndex = 1
-twoIndex = 0
+    # Initialize the index values
+    firstIndex = 1
+    secondIndex = 1
+    twoIndex = 0
 
-# Loops through both arrays and add the values in ascending order by comparison
-for twoIndex in range(len(firstArray) + len(secondArray) - 4):
-    if firstArray[firstIndex] <= secondArray[secondIndex]:
-        finalArray.append(firstArray[firstIndex])
-        firstIndex += 1
-    else:
-        finalArray.append(secondArray[secondIndex])
-        secondIndex += 1
+    # Loops through both arrays and add the values in ascending order by comparison
+    for twoIndex in range(len(firstArray) + len(secondArray) - 4):
+        if firstArray[firstIndex] <= secondArray[secondIndex]:
+            finalArray.append(firstArray[firstIndex])
+            firstIndex += 1
+        else:
+            finalArray.append(secondArray[secondIndex])
+            secondIndex += 1
+    return finalArray
 
-print(str(len(finalArray)) + " ", end = '')
+def merge_sort(anotherArray):
+    arrayLength = len(anotherArray)
+
+    # Base case for recursion
+    if arrayLength <= 1:
+        return anotherArray
+    
+    firstHalfArray = merge_sort(anotherArray[:(int(arrayLength / 2))])
+    secondHalfArray = merge_sort(anotherArray[(int(arrayLength / 2)):])
+
+    primedArray = merge_sorted(firstHalfArray, secondHalfArray)
+    
+    return primedArray
+
+anotherInput = input()
+arrayToSort = list(map(int, input().split()))
+finalArray = merge_sort(arrayToSort)
+
+# print(str(len(finalArray)) + " ", end = '')
 for num in finalArray[:-1]:
     print(str(num) + " ", end = '')
 print(str(finalArray[-1]))
