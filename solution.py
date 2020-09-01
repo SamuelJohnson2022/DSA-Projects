7374eaf8bf623b0d797d7d83de7ae62300326461
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2020-08-31 20:00:12 -0400

    added parenthesis

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 # Take the two input arrays and saves them to a and b respectively
-firstArray = list(map(int, input.split()))
-secondArray = list(map(int, input.split()))
+firstArray = list(map(int, input().split()))
+secondArray = list(map(int, input().split()))
 finalArray = []
 
 # Initialize the index values
