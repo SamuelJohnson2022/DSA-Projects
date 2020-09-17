222ac1c2fb127d8f9056ca5ea973eeeeab35a56a
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2020-09-17 17:32:49 -0400

    18th attempt on problem 1

---
+++ Assignment2/problem1/solution.py
@@ -0,0 +1,48 @@
+# Collaborators: Samuel Johnson, Nikita Petrenko, Jason Novillo
+import sys
+
+numOfSwaps = 0
+
+
+def merge_sorted(firstArray, secondArray):
+    firstArray.append(sys.maxsize)
+    secondArray.append(sys.maxsize)
+    finalArray = []
+
+    # Initialize the index values
+    firstIndex = 0
+    secondIndex = 0
+
+    # Loops through both arrays and add the values in ascending order by comparison
+    for num in range(len(firstArray) + len(secondArray) - 2):
+        if firstArray[firstIndex] <= secondArray[secondIndex]:
+            finalArray.append(firstArray[firstIndex])
+            firstIndex += 1
+        else:
+            finalArray.append(secondArray[secondIndex])
+            secondIndex += 1
+            if firstArray[firstIndex] != sys.maxsize:
+                numOfSwaps += 1
+    return finalArray
+
+
+def merge_sort(anotherArray):
+    arrayLength = len(anotherArray)
+
+    # Base case for recursion
+    if arrayLength <= 1:
+        return anotherArray
+
+    firstHalfArray = merge_sort(anotherArray[:int(arrayLength / 2)])
+    secondHalfArray = merge_sort(anotherArray[int(arrayLength / 2):])
+
+    primedArray = merge_sorted(firstHalfArray, secondHalfArray)
+
+    return primedArray
+
+
+anotherInput = input()
+arrayToSort = input().split(" ")
+finalArray = merge_sort(arrayToSort)
+
+print(numOfSwaps)
