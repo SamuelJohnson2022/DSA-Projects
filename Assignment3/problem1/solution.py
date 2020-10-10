aa01291d13e391c4991353b6d9f3a649ddef9394
Jason Novillo <jxn262@psu.edu>
2020-10-10 16:00:20 -0400

    I have a question. For God? WHY?!

--- Assignment3/problem1/solution.py
+++ Assignment3/problem1/solution.py
@@ -7,23 +7,57 @@ num_vertices = line_input[0]
 num_edges = line_input[1]
 
 graph = []
+r_graph = []
+visited = [] 
+pre = []
+post = []
+postlist = [] 
+
+for i in range(num_vertices):
+    visited.append(0)
+    pre.append(0)
+    post.append(0)
+    
+for i in range(num_vertices):
+    graph.append([])
+    r_graph.append([])
 
 for i in range(num_edges):
     n, m = list(map(int, input().split()))
-    print(n, m)
+    graph[n-1].append(m)
+    r_graph[m-1].append(n) # Build reversed graph.
+    
+clock = 1
+def DFS_time(graph):
+    global num_vertices
+    global clock 
+    global visited 
+    clock = 1
+    for i in range(num_vertices):
+        if (visited[i] == 0):
+            explore(graph, i)
+
+def explore(graph, v):
+    global visited
+    global pre 
+    global post
+    global postlist 
+    global clock
+    visited[v] = 1
+    pre[v] = clock
+    clock += 1
+    for each in graph[v]:
+        if (visited[each-1] == 0):
+            explore(graph,each-1)
+    post[v] = clock
+    clock += 1
+    postlist.append(v)
+
+
     
 
-reversed_graph = []
 
-def specificOrder(graph):
-    # Build Gr
-    for i in range(num_edges):
-        for j in range(2):
-            # swap vertices
-            reversed_graph[i][j] = graph[i][j+1]
-            reversed_graph[i][j+1] = graph[i][j]
 
-            # Run DFS on reversed_graph => post_list
-            # specific_order = post_list.reverse()
-            # return specific_order
+
+
 
