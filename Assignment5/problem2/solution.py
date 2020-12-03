2a154685072562ea9580c60aab4e87eccf24fb6d
Nikita Petrenko <thenikitapetrenko@gmail.com>
2020-12-03 17:38:56 -0500

    Save sorted edge weights

--- Assignment5/problem2/solution.py
+++ Assignment5/problem2/solution.py
@@ -1,1 +1,24 @@
+# Samuel Johnson - sdj5203
+# Nikita Petrenko - nmp5361
+# Jason Novillo - jxn262
+import sys
+
 line_input = list(map(int, input().split(" ")))
+
+# Reads
+num_vertices = line_input[0]
+num_edges = line_input[1]
+
+edge_weights = []
+
+for i in range(1, num_edges + 1):
+    line_input = list(map(int, input().split(" ")))
+    start_vertex = line_input[0]
+    end_vertex = line_input[1]
+    edge_weights.append(line_input[2])
+
+print(edge_weights)
+
+edge_weights.sort()
+
+# sort edges in increasing order of weight
