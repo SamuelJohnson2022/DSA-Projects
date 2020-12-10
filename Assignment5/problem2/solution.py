a5c715c8ed01b91b5843178d4dbc07b33832db09
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2020-12-10 16:01:33 -0500

    update problem 2

--- Assignment5/problem2/solution.py
+++ Assignment5/problem2/solution.py
@@ -3,11 +3,48 @@
 # Jason Novillo - jxn262
 import sys
 
-class Node():
-    def __init__(self, data, height, parent):
-        self.data = data
-        self.height = height
-        self.parent = parent
+
+class Edge:
+    def __init__(self, start, end, weight):
+        self.start = start
+        self.end = end
+        self.weight = weight
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
+class Set:
+
+    def __init__(self, vertex):
+        self.height = 1
+        self.parent = self
+        self.vertex = vertex
+
+    def find(self):
+        current = self
+        while (current.parent != current):
+            current = current.parent
+        return current
+
+
+def union(x, y):
+    rx = x.find()
+    ry = y.find()
+
+    if(rx == ry):
+        return
+
+    if(rx.height < ry.height):
+        rx.parent = ry
+    elif rx.height > ry.height:
+        ry.parent = rx
+    else:
+        rx.parent = ry
+        ry.height = ry.height + 1
+
+    return
+
 
 line_input = list(map(int, input().split(" ")))
 
@@ -15,29 +52,35 @@ line_input = list(map(int, input().split(" ")))
 num_vertices = line_input[0]
 num_edges = line_input[1]
 
-edge_weights = []
-
+edges = []
 # Save edge weights in a list
 for i in range(1, num_edges + 1):
     line_input = list(map(int, input().split(" ")))
-    start_vertex = line_input[0]
-    end_vertex = line_input[1]
-    edge_weights.append(line_input[2])
+    edges.append(Edge(line_input[0], line_input[1], line_input[2]))
 
 # Sort edge weights
 # print(edge_weights)
-edge_weights.sort()
+edges.sort()
 
 E1 = []
+vertex_set = []
+vertex_set.append(None)
 
-def make_set(v):
-    v.height = 1;
-    v.parent = v;
+for v in range(1, num_vertices + 1):
+    vertex_set.append(Set(v))
 
-def find(v):
-    while (v.parent != v):
-        v = v.parent
-    return v
+for e in edges:
+    # Find root of u
+    start_root = vertex_set[e.start].find()
+    end_root = vertex_set[e.end].find()
 
-for v in range(num_vertices + 1):
-    make_set(v)
+    if(start_root != end_root):
+        E1.append(e)
+        union(start_root, end_root)
+
+total = 0
+# Add up the MST weights
+for edgeee in E1:
+    total += edgeee.weight
+
+print(total)
